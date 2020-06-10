47955e8e5790e23d6d9058d760c490d0666dadad
Clara Gaset <claragaset4@gmail.com>
2020-06-10 10:22:19 +0200

    Uses rest params and destructuring in triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,12 +1,12 @@
 export class Triangle {
 
-  constructor(side1, side2, side3) {
-    this.sides = [side1, side2, side3]
+  constructor(...sides) {
+    this.sides = sides
   }
 
   isEquilateral() {
-    const firstSide = this.sides[0]
-    return this.isATriangle() && !this.sides.some(side => side != firstSide)
+    const side1 = this.sides[0]
+    return this.isATriangle() && this.sides.every(side => side == side1)
   }
 
   isIsosceles() {
@@ -14,17 +14,13 @@ export class Triangle {
   }
 
   isScalene() {
-    const side1 = this.sides[0]
-    const side2 = this.sides[1]
-    const side3 = this.sides[2]
+    const [side1, side2, side3] = this.sides
     const sidesAreDifferent = side1 != side2 && side1 != side3 && side2 != side3
     return this.isATriangle() && sidesAreDifferent
   }
 
   isATriangle() {
-    const side1 = this.sides[0]
-    const side2 = this.sides[1]
-    const side3 = this.sides[2]
+    const [side1, side2, side3] = this.sides
     const sidesSizeAreValid = side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1
     return !this.sides.some(side => side == 0) && sidesSizeAreValid
   }
